e0fb70df836dab2a586469f6dc43ba967daee741
Suradeepthi <98090338+Suradeepthi@users.noreply.github.com>
2022-03-04 01:41:16 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var yellow,red,green;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+  fill("black");
+  rect(100, 50, 100, 300); 
+  rect(140, 350, 20, 300);
+}
+
+
+function draw() {
+  
+  red = new Signal(150, 100, "red");
+  red.appear();
+  
+  //Create objects for Green and yellow lights :
+  green = new Signal(150, 200, "green");
+  green.appear();
+  yellow = new Signal(150, 300, "yellow");
+  yellow.appear();
+
+}
+    
+
